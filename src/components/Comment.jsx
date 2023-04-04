f2ba72119d097506803d739677ba19a8a8e7f947
beasou <beatrizreginasouza1@gmail.com>
2023-04-04 13:01:21 -0300

    Criando esqueleto do componente Comment

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,7 +1,37 @@
+import { ThumbsDown, ThumbsUp, Trash } from "phosphor-react";
 import styles from "./Comment.module.css";
 
-export function Comment(){
-return(
- <article className={styles.Coment}></article>   
-)
+export function Comment() {
+  return (
+    <div className={styles.comment}>
+      <img
+        src="https://github.com/beasou.png"
+      />
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Beatriz Souza</strong>
+              <time
+                title="06 de Fevereiro às 12:35"
+                dateTime="2023-02-06 12:35:49"
+              >
+                Cerca de 1h atrás
+              </time>
+            </div>
+            <button title="Deletar comentário">
+                <Trash size={20}/>
+            </button>
+          </header>
+          <p>Muito bom, parabéns!! 👏👏</p>
+        </div>
+        <footer>
+            <button>
+                <ThumbsUp/>
+                Aplaudir <span>20</span>
+            </button>
+        </footer>
+      </div>
+    </div>
+  );
 }
