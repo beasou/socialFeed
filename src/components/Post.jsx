1371bf0ae4d9725baf207a348812bda49419664f
beasou <beatrizreginasouza1@gmail.com>
2023-08-09 21:48:37 -0300

    Validando formulário

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -9,11 +9,9 @@ import styles from "./Post.module.css";
 import { useState } from "react";
 
 export function Post({ author, publishedAt, content }) {
-  const[comments, setComments] = useState([
-    'Muito bom, parabéns!! 👏👏'
-  ])
+  const [comments, setComments] = useState(["Muito bom, parabéns!! 👏👏"]);
 
-  const [newCommentText, setNewCommentText] = useState('')
+  const [newCommentText, setNewCommentText] = useState("");
 
   const publishedDateFormatted = format(
     publishedAt,
@@ -28,32 +26,37 @@ export function Post({ author, publishedAt, content }) {
     addSuffix: true,
   });
 
-  function handleCreateNewComment(){
-    event.preventDefault()
-    
-    setComments([...comments, newCommentText])
+  function handleCreateNewComment() {
+    event.preventDefault();
 
-    setNewCommentText('')
+    setComments([...comments, newCommentText]);
 
+    setNewCommentText("");
   }
-  function handleNewCommentChange(){
-    setNewCommentText(event.target.value)
-
+  function handleNewCommentChange() {
+    event.target.setCustomValidity("");
+    setNewCommentText(event.target.value);
   }
 
-  function deleteComment(commentToDelete){
-    const commentsWithoutDeletedOne = comments.filter(comment =>{
+  function handleNewCommentInvalid() {
+    event.target.setCustomValidity("Esse campo é obrigatório!");
+  }
+
+  function deleteComment(commentToDelete) {
+    const commentsWithoutDeletedOne = comments.filter((comment) => {
       return comment !== commentToDelete;
-    })
-    
+    });
+
     setComments(commentsWithoutDeletedOne);
   }
 
+  const isNewCommentEmpty = newCommentText.length === 0
+  
   return (
     <article className={styles.post}>
       <header>
         <div className={styles.author}>
-          <Avatar src={author.avatarUrl}/>
+          <Avatar src={author.avatarUrl} />
           <div className={styles.authorInfo}>
             <strong>{author.name}</strong>
             <span>{author.role}</span>
@@ -68,35 +71,45 @@ export function Post({ author, publishedAt, content }) {
       </header>
 
       <div className={styles.content}>
-        {content.map(line => {
-          if(line.type == 'paragraph'){
+        {content.map((line) => {
+          if (line.type == "paragraph") {
             return <p key={line.content}>{line.content}</p>;
-          }else if (line.type == 'link'){
-            return <p key={line.content}><a href="#">{line.content}</a></p>
+          } else if (line.type == "link") {
+            return (
+              <p key={line.content}>
+                <a href="#">{line.content}</a>
+              </p>
+            );
           }
         })}
       </div>
 
       <form onSubmit={handleCreateNewComment} className={styles.commentForm}>
         <strong>Deixe seu feedback</strong>
-        <textarea 
-          type="text" 
+        <textarea
+          type="text"
           placeholder="Escreva um comentário"
-          value={newCommentText}  
+          value={newCommentText}
           name="comment"
           onChange={handleNewCommentChange}
+          onInvalid={handleNewCommentInvalid}
+          required
         />
         <footer>
-          <button type="submit">Publicar</button>
+          <button type="submit" disabled={isNewCommentEmpty}>
+            Publicar
+          </button>
         </footer>
       </form>
       <div className={styles.commentList}>
-        {comments.map(comment => {
-          return <Comment 
-                    key={comment}
-                    content={comment}
-                    onDeleteComment={deleteComment}
-                  />
+        {comments.map((comment) => {
+          return (
+            <Comment
+              key={comment}
+              content={comment}
+              onDeleteComment={deleteComment}
+            />
+          );
         })}
       </div>
     </article>
